75cb79caeb0d9a20f2cccc027aa5c523ec8580c4
tranglgranville <54959772+tranglgranville@users.noreply.github.com>
2023-10-12 12:12:20 -0700

    Add files via upload

---
+++ MobileApp/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MobileApp.View;
+using MobileApp.Services;
+
+namespace MobileApp
+{
+    public partial class App : Application
+    {
+        
+        public App()
+        {
+            InitializeComponent();
+
+            if (Settings.FirstRun)
+            {
+                SQLiteDB.LoadSampleData();
+                //SQLiteDB.LoadSyncSampleData();
+                Settings.FirstRun = false;
+            }
+            var termMainPage = new TermMainPage();
+            var navPage = new NavigationPage(termMainPage);
+            MainPage = navPage;
+        }
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
